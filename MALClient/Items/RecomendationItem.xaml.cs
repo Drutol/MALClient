0d4ff47e83ecfad050560074d685fef4ae01d17d
Piotr Szymczak <dogier140@poczta.fm>
2016-04-23 22:01:58 +0200

    recomm wip update

--- MALClient/Items/RecomendationItem.xaml.cs
+++ MALClient/Items/RecomendationItem.xaml.cs
@@ -16,7 +16,7 @@ namespace MALClient.Items
     public sealed partial class RecomendationItem : UserControl
     {
         private readonly RecomendationData _data;
-        private readonly ObservableCollection<ListViewItem> _detailItems = new ObservableCollection<ListViewItem>();
+        private readonly ObservableCollection<Tuple<string,string,string,string,string>> _detailItems = new ObservableCollection<Tuple<string,string, string, string, string>>();
         private bool _dataLoaded;
 
         public RecomendationItem(RecomendationData data, int index)
@@ -55,50 +55,19 @@ namespace MALClient.Items
             TxtDepTitle.Text = _data.DependentTitle;
             TxtRecTitle.Text = _data.RecommendationTitle;
             TxtRecommendation.Text = _data.Description;
-            _detailItems.Add(BuildListViewItem("Episodes", _data.DependentEpisodes, _data.RecommendationEpisodes));
-            _detailItems.Add(BuildListViewItem("Score", _data.DependentGlobalScore.ToString(),
-                _data.RecommendationGlobalScore.ToString()));
-            _detailItems.Add(BuildListViewItem("Type", _data.DependentType, _data.RecommendationType));
-            _detailItems.Add(BuildListViewItem("Status", _data.DependentStatus, _data.RecommendationStatus));
-            _detailItems.Add(BuildListViewItem("Start:", _data.DependentStartDate, _data.RecommendationStartDate));
-            _detailItems.Add(BuildListViewItem("End:", _data.DependentEndDate, _data.RecommendationStartDate));
+
+            var myDepItem = await ViewModelLocator.AnimeList.TryRetrieveAuthenticatedAnimeItem(_data.DependentId);
+            var myRecItem = await ViewModelLocator.AnimeList.TryRetrieveAuthenticatedAnimeItem(_data.RecommendationId);
+
+            _detailItems.Add(new Tuple<string, string, string, string,string>("Episodes", _data.DependentEpisodes,myDepItem?.MyEpisodes.ToString() ?? "", _data.RecommendationEpisodes, myRecItem?.MyEpisodes.ToString() ?? ""));
+            _detailItems.Add(new Tuple<string, string, string, string, string>("Score", _data.DependentGlobalScore.ToString(), myDepItem?.MyScore.ToString() ?? "", _data.RecommendationGlobalScore.ToString(), myRecItem?.MyScore.ToString() ?? ""));
+            _detailItems.Add(new Tuple<string, string, string, string, string>("Type", _data.DependentType,"", _data.RecommendationType,""));
+            _detailItems.Add(new Tuple<string, string, string, string, string>("Status", _data.DependentStatus,"", _data.RecommendationStatus,""));
+            _detailItems.Add(new Tuple<string, string, string, string, string>("Start:", _data.DependentStartDate, myDepItem?.StartDate ?? "", _data.RecommendationStartDate, myRecItem?.StartDate ?? ""));
+            _detailItems.Add(new Tuple<string, string, string, string, string>("End:", _data.DependentEndDate, myDepItem?.EndDate ?? "", _data.RecommendationStartDate, myRecItem?.EndDate?? ""));
             DetailsListView.ItemsSource = _detailItems;
             _dataLoaded = true;
             SpinnerLoading.Visibility = Visibility.Collapsed;
-        }
-
-        private ListViewItem BuildListViewItem(string label, string val1, string val2)
-        {
-            return new ListViewItem
-            {
-                Content = new Grid
-                {
-                    ColumnDefinitions =
-                    {
-                        new ColumnDefinition {Width = new GridLength(0.24, GridUnitType.Star)},
-                        new ColumnDefinition {Width = new GridLength(0.38, GridUnitType.Star)},
-                        new ColumnDefinition {Width = new GridLength(0.38, GridUnitType.Star)}
-                    },
-                    Children =
-                    {
-                        BuildTextBlock(label, FontWeights.SemiBold, 0),
-                        BuildTextBlock(val1, FontWeights.SemiLight, 1),
-                        BuildTextBlock(val2, FontWeights.SemiLight, 2)
-                    }
-                }
-            };
-        }
-
-        private TextBlock BuildTextBlock(string value, FontWeight weight, int column)
-        {
-            var txt = new TextBlock
-            {
-                Text = value,
-                FontWeight = weight,
-                TextAlignment = !weight.Equals(FontWeights.SemiBold) ? TextAlignment.Center : TextAlignment.Left
-            };
-            txt.SetValue(Grid.ColumnProperty, column);
-            return txt;
         }
 
         private async void ButtonRecomDetails_OnClick(object sender, RoutedEventArgs e)

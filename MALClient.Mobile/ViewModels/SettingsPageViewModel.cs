60a5751c566c9b5f4d24b3d9edd19c6dd0084522
Piotr Szymczak <dogier140@poczta.fm>
2016-07-29 22:05:12 +0200

    v4.4.7(8).0 - internal

--- MALClient.Mobile/ViewModels/SettingsPageViewModel.cs
+++ MALClient.Mobile/ViewModels/SettingsPageViewModel.cs
@@ -340,6 +340,12 @@ namespace MALClient.ViewModels
             set { Settings.SyncFavsFromTimeToTime = value; }
         }
 
+        public static bool EnsureRandomizerAlwaysSelectsWinner
+        {
+            get { return Settings.EnsureRandomizerAlwaysSelectsWinner; }
+            set { Settings.EnsureRandomizerAlwaysSelectsWinner = value; }
+        }
+
         public static bool MangaFocusVolumes
         {
             get { return Settings.MangaFocusVolumes; }

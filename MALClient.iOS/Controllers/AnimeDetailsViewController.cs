5ad0fa8af60b80ff83dd5364143cde892a9697df
Łukasz Zimoń <lukasz.zimon@outlook.com>
2016-09-28 00:13:39 +0200

    AnimeDetailsView basics

---
+++ MALClient.iOS/Controllers/AnimeDetailsViewController.cs
@@ -0,0 +1,13 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace MALClient.iOS
+{
+    public partial class AnimeDetailsViewController : UIViewController
+    {
+        public AnimeDetailsViewController (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}

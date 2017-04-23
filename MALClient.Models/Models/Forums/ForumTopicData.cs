80eb76fb41127ec0c84898746336bd1d44253fe3
Piotr Szymczak <dogier140@poczta.fm>
2017-04-23 18:46:24 +0200

    Added tests

--- MALClient.Models/Models/Forums/ForumTopicData.cs
+++ MALClient.Models/Models/Forums/ForumTopicData.cs
@@ -8,6 +8,36 @@ namespace MALClient.Models.Models.Forums
 {
     public class ForumTopicData
     {
+        protected bool Equals(ForumTopicData other)
+        {
+            return AllPages == other.AllPages && CurrentPage == other.CurrentPage &&
+                   string.Equals(Title, other.Title) && IsLocked == other.IsLocked &&
+                   (string.Equals(TargetMessageId, other.TargetMessageId) || string.IsNullOrEmpty(TargetMessageId) && string.IsNullOrEmpty(other.TargetMessageId)) && string.Equals(Id, other.Id) &&
+                   other.Messages.All(entry => Messages.Any(messageEntry => messageEntry.Id == entry.Id));
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((ForumTopicData) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var hashCode = AllPages;
+                hashCode = (hashCode * 397) ^ CurrentPage;
+                hashCode = (hashCode * 397) ^ (Title != null ? Title.GetHashCode() : 0);
+                hashCode = (hashCode * 397) ^ IsLocked.GetHashCode();
+                hashCode = (hashCode * 397) ^ (TargetMessageId != null ? TargetMessageId.GetHashCode() : 0);
+                hashCode = (hashCode * 397) ^ (Id != null ? Id.GetHashCode() : 0);
+                return hashCode;
+            }
+        }
+
         public List<ForumMessageEntry> Messages { get; set; } = new List<ForumMessageEntry>();
         public int AllPages { get; set; }
         public int CurrentPage { get; set; }
---
+++ UnitTestProject1/Tests.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight.Ioc;
+using MALClient.Adapters;
+using MALClient.Adapters.Credentials;
+using MALClient.Models.Enums;
+using MALClient.Models.Models.Forums;
+using MALClient.WPF;
+using MALClient.WPF.Adapters;
+using MALClient.XShared.Comm;
+using MALClient.XShared.Comm.MagicalRawQueries.Forums;
+using MALClient.XShared.Utils;
+using MALClient.XShared.ViewModels;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using Assert = NUnit.Framework.Assert;
+using CollectionAssert = NUnit.Framework.CollectionAssert;
+using DataCache = MALClient.WPF.Adapters.DataCache;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class Tests
+    {
+        public Tests()
+        {
+            ViewModelLocator.RegisterBase();
+            ResourceLocator.RegisterBase();
+            SimpleIoc.Default.Register<IDataCache, DataCache>();
+            SimpleIoc.Default.Register<IPasswordVault, PasswordVaultProvider>();
+            SimpleIoc.Default.Register<IApplicationDataService, ApplicationDataServiceService>();
+            SimpleIoc.Default.Register<IDatabaseContextProvider, DatabaseContextProvider>();
+
+            Credentials.Init();
+        }
+
+        [TestCase]
+        public async Task TestDatabaseCache()
+        {
+            var dbProvider = ResourceLocator.DatabaseService;
+
+            var library = await new LibraryListQuery("MALClientTestAcc", AnimeListWorkModes.Anime).GetLibrary(true);
+            await Task.Delay(2000);
+            var cached = await dbProvider.RetrieveShowsForUser("MALClientTestAcc", true);
+
+            foreach (var libraryData in cached)
+            {
+                if (!library.Any(data => data.Id == libraryData.Id))
+                    Assert.Fail();
+            }
+        }
+
+        [TestCase]
+        public async Task TestSerialization()
+        {
+            var posts = await ForumTopicQueries.GetTopicData("1499207", 1);
+            var serialized = new MyFancyDataSerializer().SerializeObject(posts, typeof(ForumTopicData));
+            var deserialized = new MyFancyDataSerializer().DeserializeObject(serialized, typeof(ForumTopicData)) as ForumTopicData;
+
+            Assert.True(deserialized.Equals(posts));
+        }
+    }
+}

fe8963620a45794cda537c5dfead8cdb175d99b4
Guerra24 <pablo230699@hotmail.com>
2018-07-08 20:45:25 -0600

    Working NavView.

--- MALClient.Desktop/MainPageV2.xaml.cs
+++ MALClient.Desktop/MainPageV2.xaml.cs
@@ -19,6 +19,12 @@ using MALClient.UWP.ViewModels;
 using MALClient.UWP.Shared.ViewModels;
 using MALClient.UWP.Shared.UserControls;
 using MALClient.XShared.ViewModels;
+using MALClient.XShared.Utils;
+using MALClient.Models.Enums;
+using MALClient.XShared.NavArgs;
+using MALClient.XShared.Comm.Anime;
+using MALClient.XShared.ViewModels.Main;
+using System.Diagnostics;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -29,6 +35,9 @@ namespace MALClient.UWP
 	/// </summary>
 	public sealed partial class MainPageV2 : Page, IMainViewInteractions
 	{
+
+		public Visibility LogInButtonVisibility => Credentials.Authenticated ? Visibility.Collapsed : Visibility.Visible;
+
 		public MainPageV2()
 		{
 			this.InitializeComponent();
@@ -53,6 +62,55 @@ namespace MALClient.UWP
 				//ViewModelLocator.Settings.OnAdsMinutesPerDayChanged += SettingsOnOnAdsMinutesPerDayChanged;
 				ViewModelLocator.GeneralMain.ChangelogVisibility = ResourceLocator.ChangelogProvider.NewChangelog;
 			};
+		}
+
+		private void NavView_ItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
+		{
+			if (args.IsSettingsInvoked)
+			{
+				DesktopViewModelLocator.Main.Navigate(PageIndex.PageSettings);
+			}
+			else
+			{
+				PageIndex page;
+				var tag = sender.MenuItems
+						.OfType<NavigationViewItem>()
+						.First(i => args.InvokedItem.Equals(i.Content))
+						.Tag.ToString();
+				if (Enum.TryParse(tag as string, out page))
+				{
+					DesktopViewModelLocator.Main.Navigate(page, GetAppropriateArgsForPage(page));
+				}
+			}
+		}
+
+		private object GetAppropriateArgsForPage(PageIndex page)
+		{
+			switch (page)
+			{
+				case PageIndex.PageSeasonal:
+					return AnimeListPageNavigationArgs.Seasonal;
+				case PageIndex.PageMangaList:
+					return AnimeListPageNavigationArgs.Manga;
+				case PageIndex.PageMangaSearch:
+					return new SearchPageNavigationArgs { Anime = false };
+				case PageIndex.PageSearch:
+					return new SearchPageNavigationArgs();
+				case PageIndex.PageTopAnime:
+					return AnimeListPageNavigationArgs.TopAnime(TopAnimeType.General);
+				case PageIndex.PageTopManga:
+					return AnimeListPageNavigationArgs.TopManga;
+				case PageIndex.PageProfile:
+					return new ProfilePageNavigationArgs { TargetUser = Credentials.UserName };
+				case PageIndex.PageArticles:
+					return MalArticlesPageNavigationArgs.Articles;
+				case PageIndex.PageNews:
+					return MalArticlesPageNavigationArgs.News;
+				case PageIndex.PageWallpapers:
+					return new WallpaperPageNavigationArgs();
+				default:
+					return null;
+			}
 		}
 
 		private void Navigate(Type page, object args = null)
@@ -84,10 +142,10 @@ namespace MALClient.UWP
 
 		private void Page_SizeChanged(object sender, SizeChangedEventArgs e)
 		{
-			if (e.NewSize.Width <= 640)
+			if (e.NewSize.Width <= 960)
 				MainSplitView.OpenPaneLength = e.NewSize.Width;
 			else
-				MainSplitView.OpenPaneLength = 500;
+				MainSplitView.OpenPaneLength = 535;
 
 		}
 
@@ -105,6 +163,7 @@ namespace MALClient.UWP
 
 		public void SearchInputFocus(FocusState state)
 		{
+			SearchInput.Focus(state);
 		}
 
 	}

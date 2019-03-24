cc027c8fb1a26300b12e3dfa4ab35805ff9349ea
Piotr Szymczak <dogier140@poczta.fm>
2019-03-24 13:42:02 +0100

    UWP v5.5.6.0

--- MALClient.UWP.Adapters/ChangeLogProvider.cs
+++ MALClient.UWP.Adapters/ChangeLogProvider.cs
@@ -13,12 +13,12 @@ namespace MALClient.UWP.Adapters
         public bool NewChangelog { get; set; }
         public string CurrentVersion => UWPUtilities.GetAppVersion();
 
-        public string DateWithVersion =>  $"v{UWPUtilities.GetAppVersion()} - 07.10.2018";
+        public string DateWithVersion =>  $"v{UWPUtilities.GetAppVersion()} - 24.03.2019";
 
         public List<string> Changelog => new List<string>
         {
-            "Fixed setting dates.",
-            "Now you can also edit dates for manga.",
+            "Overall update, now on par with Android version.",
+            "Removed ads."
         };
     }
 }

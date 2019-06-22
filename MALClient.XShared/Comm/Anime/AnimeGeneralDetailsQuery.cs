42987812133f007502bfb8d1877d51e1fdc3f48d
Piotr Szymczak <dogier140@poczta.fm>
2019-06-22 12:56:52 +0200

    Fix broken details cause invalid json

--- MALClient.XShared/Comm/Anime/AnimeGeneralDetailsQuery.cs
+++ MALClient.XShared/Comm/Anime/AnimeGeneralDetailsQuery.cs
@@ -217,52 +217,28 @@ namespace MALClient.XShared.Comm.Anime
         [Preserve(AllMembers = true)]
         public class RootObject
         {
-            public string request_hash { get; set; }
-            public bool request_cached { get; set; }
-            public int request_cache_expiry { get; set; }
             public int mal_id { get; set; }
-            public string url { get; set; }
             public string image_url { get; set; }
-            public string trailer_url { get; set; }
             public string title { get; set; }
             public string title_english { get; set; }
             public string title_japanese { get; set; }
             public List<string> title_synonyms { get; set; }
             public string type { get; set; }
-            public string source { get; set; }
             public int? episodes { get; set; }
             public string status { get; set; }
-            public bool airing { get; set; }
             public Aired aired { get; set; }
-            public string duration { get; set; }
-            public string rating { get; set; }
+
             public double? score { get; set; }
-            public int scored_by { get; set; }
-            public int? rank { get; set; }
-            public int popularity { get; set; }
-            public int members { get; set; }
-            public int favorites { get; set; }
+
             public string synopsis { get; set; }
-            public string background { get; set; }
-            public string premiered { get; set; }
-            public string broadcast { get; set; }
-            public Related related { get; set; }
-            public List<Producer> producers { get; set; }
-            public List<Licensor> licensors { get; set; }
-            public List<Studio> studios { get; set; }
-            public List<Genre> genres { get; set; }
-            public List<string> opening_themes { get; set; }
-            public List<string> ending_themes { get; set; }
         }
 
         [Preserve(AllMembers = true)]
         class MangaRootObject
         {
-            public string request_hash { get; set; }
-            public bool request_cached { get; set; }
-            public int request_cache_expiry { get; set; }
+
             public int mal_id { get; set; }
-            public string url { get; set; }
+
             public string title { get; set; }
             public string title_english { get; set; }
             public List<string> title_synonyms { get; set; }
@@ -272,20 +248,10 @@ namespace MALClient.XShared.Comm.Anime
             public string type { get; set; }
             public int? volumes { get; set; }
             public int? chapters { get; set; }
-            public bool publishing { get; set; }
             public Published published { get; set; }
-            public int rank { get; set; }
             public double? score { get; set; }
-            public int scored_by { get; set; }
-            public int popularity { get; set; }
-            public int members { get; set; }
-            public int? favorites { get; set; }
+
             public string synopsis { get; set; }
-            public string background { get; set; }
-            public Related related { get; set; }
-            public List<Genre> genres { get; set; }
-            public List<Author> authors { get; set; }
-            public List<Serialization> serializations { get; set; }
         }
     }
 }
